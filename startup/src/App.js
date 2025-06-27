909ec4bc513db0979bb225583f5ca1d26d3c0a63
Tolulope McSmith <tolulope.mcsmith@gmail.com>
2025-06-27 18:43:25 +0100

    Code Clean Up

--- startup/src/App.js
+++ startup/src/App.js
@@ -1,6 +1,9 @@
+import React from 'react';
 import './App.css';
 import Home from './Pages/Home';
 import Portfolio from './Pages/Portfolio';
+import Services from './Pages/Services';
+import Contact from './Pages/Contact';
 import NavBar from './Components/NavBar';
 import FooterSection from './Components/FooterSection';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -8,14 +11,18 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <Router>
-    <div className="App">
-      <NavBar />
-      <Home />
-      {/* <Portfolio/> */}
-      <FooterSection />
-    </div>
+      <div className="App">
+        <NavBar /> 
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/portfolio' element={<Portfolio />} />
+          <Route path='/services' element={<Services />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+        <FooterSection /> 
+      </div>
     </Router>
   );
-}
+} 
 
 export default App;
