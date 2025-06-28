06d921cbead4b04d12ea74f86c59de1c228ccabb
Tolulope McSmith <tolulope.mcsmith@gmail.com>
2025-06-28 14:50:40 +0100

    Form4Details jsx

--- startup/src/Pages/Contact.js
+++ startup/src/Pages/Contact.js
@@ -1,12 +1,18 @@
 import './Contact.css'
+import ContactIntro from '../Components/ContactIntro';
+import GoogleMap from '../Components/GoogleMap';
+import Form4Details from '../Components/Form4Details';
 
 
 
 const Contact = () => {
     return ( 
-        <div></div>
+        <div>
+            <ContactIntro/>
+            <GoogleMap/>
+            <Form4Details/>
+        </div>
     );
 }
-
 
 export default Contact;
