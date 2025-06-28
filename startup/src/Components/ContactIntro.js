4cd2adcdd35de8241748356c893ba6a6b70cc9f5
Tolulope McSmith <tolulope.mcsmith@gmail.com>
2025-06-28 12:58:10 +0100

    CSS Rules for ContactIntro Section

---
+++ startup/src/Components/ContactIntro.js
@@ -0,0 +1,13 @@
+import './ContactIntro.css'
+
+const ContactIntro = () => {
+    return ( 
+        <div className='contactintro'>
+            <h1>Contact Us</h1>
+            <p>Lorem ipsum, dolor sit amet 
+            <br/>consectetur
+            <br/>adipisicing elit.</p>
+        </div>
+    );
+}
+export default ContactIntro;
